215dcf3473dc9f6cf5a9015927a7f6a0bd209631
cio1312 <60284639+cio1312@users.noreply.github.com>
2020-03-23 12:46:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import List from "./List.js";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<List />, rootElement);
