16bd9c99a5ef73fbb5b3a1ddc4ff0898578b91b1
Valencio ryan Fernandes <>
2022-09-05 03:42:47 +0530

    initial commit react

---
+++ build/precache-manifest.98616f4935f0722ff55f256dc91552f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8097734bc21ac0a5be7f0098231bef3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdaa330f1231f57da07c",
+    "url": "/static/css/main.dbfad6e4.chunk.css"
+  },
+  {
+    "revision": "f539da34def5962c656d",
+    "url": "/static/js/2.8c40944c.chunk.js"
+  },
+  {
+    "revision": "cdaa330f1231f57da07c",
+    "url": "/static/js/main.79a924a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
