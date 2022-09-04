42b3b5255edfde528515c562de354d5ee347a3b1
Valencio ryan Fernandes <>
2022-09-05 03:43:13 +0530

    Updates

---
+++ precache-manifest.5072765518f8f963f69238604b79c002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577e22feb45057f3acdf3fa0eac19d48",
+    "url": "/To_do_list/index.html"
+  },
+  {
+    "revision": "cdaa330f1231f57da07c",
+    "url": "/To_do_list/static/css/main.dbfad6e4.chunk.css"
+  },
+  {
+    "revision": "f539da34def5962c656d",
+    "url": "/To_do_list/static/js/2.8c40944c.chunk.js"
+  },
+  {
+    "revision": "cdaa330f1231f57da07c",
+    "url": "/To_do_list/static/js/main.79a924a9.chunk.js"
+  },
+  {
+    "revision": "4f197b7d438104e20e7c",
+    "url": "/To_do_list/static/js/runtime~main.1cf42b59.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98616f4935f0722ff55f256dc91552f6.js"
+  "/To_do_list/precache-manifest.5072765518f8f963f69238604b79c002.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/To_do_list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
